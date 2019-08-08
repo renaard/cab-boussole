e60e86268a734a4110b2a2f405eb2014b9ae7131
Jenny Martinez <me@lajennylove.com>
2019-08-08 16:52:01 -0500

    La Boussole
    First commit

---
+++ index.php
@@ -0,0 +1,91 @@
+<!doctype html>
+<html lang="fr">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- CSS Styles -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900&display=swap" rel="stylesheet">
+
+    <title>La Boussole en ligne | Outil de référence |Centre d'action bénévole de Valleyfield</title>
+  </head>
+  <body>
+  	
+  	<!-- Logo -->
+	<div class="container">
+		<div class="row">
+		    <div class="col-md-2 col-sm-2"><img class="brandlogo" src="img/logo.png" alt="La Boussole en ligne"></div>
+		    <div class="col-md-8"></div>
+		    <div class="col-md-2 col-sm-2"><img class="ans" src="img/35ans.png" alt="35 ans"></div>
+		</div>
+	</div>
+  	
+  	<!-- Nav Menu -->
+	<nav class="navbar navbar-expand-lg navbar-dark bg-blue static-top">
+	  <div class="container">
+	    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+	          <span class="navbar-toggler-icon"></span>
+	        </button>
+	    <div class="collapse navbar-collapse" id="navbarResponsive">
+	      <ul class="navbar-nav ml-auto">
+	        <li class="nav-item active">
+	          <a class="nav-link" href="#">Accueil
+                <span class="sr-only">(current)</span>
+              </a>
+	        </li>
+	        <li class="nav-item">
+	          <a class="nav-link" href="#">Qui sommes-nous</a>
+	        </li>
+	        <li class="nav-item">
+	          <a class="nav-link" href="#">Services à la population</a>
+	        </li>
+	        <li class="nav-item">
+	          <a class="nav-link" href="#">Services aux bénévoles</a>
+	        </li>
+	        <li class="nav-item">
+	          <a class="nav-link" href="#">Services aux organismes</a>
+	        </li>
+	        <li class="nav-item">
+	          <a class="nav-link" href="#">Boussole en ligne</a>
+	        </li>
+	        <li class="nav-item">
+	          <a class="nav-link" href="#">Nous joindre</a>
+	        </li>
+	      </ul>
+	    </div>
+	  </div>
+	</nav>
+	
+	<!-- Page Content -->
+	<section class="mt-img">
+		<div class="container mtop">
+		  	<div class="row">
+			  <div class="col-md-1"></div>
+			  <div class="col-md-5">
+				<h1 class="mt-4">Boussole en ligne</h1>
+				<div class="mt-line"></div>
+		  		<p class="disclamer">Que vous soyez intéressé par le bénévolat ou encore que vous cherchiez du soutien, «La boussole» vous permet de mieux connaître les organismes communautaires et les établissements du Réseau de la Santé et des Services Sociaux de la région. Nouveauté pour 2019, vous pouvez maintenant rechercher les organismes selon leur territoire, soit la MRC de Beauharnois-Salaberry et celle du Haut-St-Laurent. De plus, pour affiner votre recherche vous pouvez consulter l’index par besoin ou alors l’index  alphabétique.Vous y trouverez aussi une liste de certains numéros d’urgence, incluant des lignes sans frais.</p>
+		  		<p class="disclamer">Nous espérons que le tout facilitera votre orientation à l’intérieur de ce vaste réseau de ressources de votre communauté. Si vous voulez une version papier, communiquez avec nous au 450-373-2111.</p>
+		  		
+		  		<h2 class="mt-mg">CONSULTEZ:</h2>
+		  		
+		  		<button type="button" class="btn btn-primary btn-lg btn-blue">Beauharnois-Salaberry</button>
+		  		<button type="button" class="btn btn-primary btn-lg btn-blue">Haut-Saint-Laurent</button>
+			  </div>
+			  <div class="col-md-6"></div>	  
+			</div>
+		</div>
+	</section>
+
+    <!-- JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
